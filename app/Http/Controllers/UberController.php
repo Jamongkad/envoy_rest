668a2d7df5618663f2e91eb8325daccdb03eb5c9
jamongkad <wrm932@gmail.com>
2015-12-10 18:04:20 +0800

    updting

---
+++ app/Http/Controllers/UberController.php
@@ -0,0 +1,54 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+use App\Http\Requests;
+use App\Http\Controllers\Controller;
+
+class UberController extends Controller
+{
+
+    public function __construct(Request $request) {
+        $this->bearerToken = $request->session()->get('uber.token');
+        $this->refreshToken = $request->session()->get('uber.refresh_token');
+
+        $this->prod_url = 'https://api.uber.com/';
+        $this->sandbox_url = 'https://sandbox-api.uber.com/';
+    }
+
+    public function get_products(Request $request) { 
+
+        $curl = new \Curl\Curl();
+        $curl->setHeader('Authorization', 'Bearer ' . $this->bearerToken);
+        $curl->get($this->sandbox_url.'v1/products', [
+           'latitude' => '14.554729',
+           'longitude' => '121.024445'
+        ]);
+
+        $data = json_decode($curl->response);
+
+        dd($data);
+    }
+
+    public function make_request(Request $request) {
+        $curl = new \Curl\Curl();
+        $curl->setHeader('Authorization', 'Bearer ' . $this->bearerToken);
+        $curl->setHeader('Content-Type', 'application/json');
+
+        $params = [ 
+            'product_id' => 'feab4fca-ea1b-4c3d-b557-9add0f486c73',
+            'start_latitude' => '14.557112',
+            'start_longitude'  => '121.018889',
+            'end_latitude' => '14.558607',
+            'end_longitude' => '121.027386',
+        ];
+
+        $curl->post($this->sandbox_url.'v1/requests', json_encode($params));
+
+        $data = json_decode($curl->response);
+
+        dd($data);
+    }
+}
